07f15d9f46850aec4293122993b040e6d42ee968
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-08-04 21:12:23 +0300

    Added: contact and gallery

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, contact, gallery
 
 urlpatterns = [
     path("", index, name="index"),
+    path("gallery/", gallery, name="gallery"),
+    path("contact-us/", contact, name="contact"),
 ]
