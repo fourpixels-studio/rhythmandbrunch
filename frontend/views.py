870f2fce1822130965b591a485edaa1391bf41ec
Four Pixels Studio <121856163+fourpixels-studio@users.noreply.github.com>
2025-08-04 21:12:05 +0300

    Added: contact and gallery

--- frontend/views.py
+++ frontend/views.py
@@ -1,8 +1,28 @@
 from django.shortcuts import render
+from events.models import Event
 
 
 def index(request):
+    events = Event.objects.all()
+    latest_event = Event.objects.last()
     context = {
+        "events": events,
         "title_tag": "Home",
+        "latest_event": latest_event,
     }
     return render(request, "index.html", context)
+
+
+def contact(request):
+    context = {
+        "title_tag": "Contact Us",
+    }
+    return render(request, "contact.html", context)
+
+
+def gallery(request):
+    context = {
+        "title_tag": "Gallery",
+        "meta_description": "gallery",
+    }
+    return render(request, "gallery.html", context)
